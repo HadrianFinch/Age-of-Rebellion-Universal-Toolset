22f051ddbe533b2307bd17690c3e9c51b4fcd929
Hadrian Finch <hadrianfinch@outlook.com>
2022-01-03 21:35:23 -0800

    Add complete skills

--- js/character.js
+++ js/character.js
@@ -1,9 +1,46 @@
-var abilitys = [];
-var skills = [];
+var abilitys = [1, 2, 3, 2, 2, 1];
+var skills = 
+[
+    1,
+    0,
+    1,
+    0,
+    2,
+    0,
+    0,
+    1,
+    0,
+    0,
+    2,
+    0,
+    0,
+    2,
+    0,
+    2,
+    0,
+    0,
+    1,
+    0,
+    0,
+    1,
+    1,
+    0,
+    0,
+    0,
+    0,
+    0,
+    1,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0
+];
 var soak = 0;
-var wounds = [];
-var strain = [];
-var deffense = [];
+var wounds = [11, 0];
+var strain = [12, 0];
+var deffense = [0, 0];
 
 document.querySelector('#rollBtn').onclick = rollCheck;
 init();
@@ -19,6 +56,24 @@ function init()
         elm.onmouseout = hideRollPopup;
     }
     rollPopup.onmouseout = mouseLeftRollPopup;
+
+    // Auto add ▰▰▱▱▱
+    var rollerListElements = document.querySelectorAll('ul[class="skillsList"] li');
+    for (let i = 0; i < skills.length; i++) 
+    {
+        var elm = rollerListElements[i];
+        var text = "";
+        for (let a = 0; a < skills[i]; a++) 
+        {
+            text = text + "▰";
+        }
+
+        for (let b = 0; b < (5 - skills[i]); b++) 
+        {
+            text = text + "▱";
+        }
+        elm.innerHTML = elm.innerHTML + text;
+    }
 }
 var mouseOverRollPopup = false;
 
@@ -31,8 +86,18 @@ function mouseLeftRollPopup()
 function showRollPopup(e)
 {
     var popup = document.querySelector('#rollPopup');
-    popup.style.left = e.clientX + "pt";
-    popup.style.bottom = (e.clienty - 5) + "pt";
+    // console.log("Y: " + e.screenY);
+    if (e.screenY < 100) 
+    {
+        popup.style.bottom = "";
+        popup.style.top = (e.screenY + 20 + document.documentElement.scrollTop) + "px";
+    }
+    else
+    {
+        popup.style.top = "";
+        popup.style.bottom = ((window.innerHeight - e.screenY) - 20 - document.documentElement.scrollTop) + "px";
+    }
+    popup.style.left = e.screenX + "px";
     popup.style.display = 'block';
 }
 
@@ -50,6 +115,9 @@ function hideRollPopup(e)
 
 function rollCheck()
 {
+    mouseOverRollPopup = false;
+    hideRollPopup();
+
     var proficency = 2; // TODO: make based on skill
     var ability = 1; // TODO: Make based on ability - skill
 
