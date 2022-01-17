aadcae31abc3e5eb4c6211abb1a346eb6fd5b94b
Hadrian Finch <hadrianfinch@outlook.com>
2022-01-16 16:54:27 -0800

    add species picker

--- js/character-builder.js
+++ js/character-builder.js
@@ -4,16 +4,39 @@ function init()
     document.querySelector('#defaultOpen').click();
     var rs = getComputedStyle(document.querySelector(':root'));
     document.querySelector('.topBox input[type="color"]').value = rs.getPropertyValue('--themeColor');
+
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) 
+    {
+        coll[i].addEventListener("click", function() 
+        {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.maxHeight)
+            {
+                content.style.maxHeight = null;
+            }
+            else 
+            {
+                content.style.maxHeight = content.scrollHeight + "px";
+            } 
+        });
+    }
 }
+
 document.querySelector('.topBox input[type="color"]').oninput = function()
 {
     setThemeColor(this.value);
 }
+
 function setThemeColor(color)
 {
     document.querySelector(':root').style.setProperty('--themecolor', color);
     localStorage.setItem("themeColor", color);
 }
+
 function openTab(evt, tabName) 
 {
     var i, tabcontent, tablinks;
@@ -29,4 +52,16 @@ function openTab(evt, tabName)
     }
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
+}
+
+document.querySelector('#Species select').oninput = function()
+{
+    var elms = document.querySelectorAll("div.raceOption");
+    for (let i = 0; i < elms.length; i++)
+    {
+        var elm = elms[i];
+        elm.style.display = null;
+    }
+
+    document.querySelector('#' + this.value).style.display = 'block';
 }
